142a5a23669a1f2df4750b8164c0cf15fc4f8e6e
eunkangchoi <ekk12mv2@gmail.com>
2023-03-21 16:08:06 +0900

    :children_crossing: chore: swagger 추가
    스웨거모듈 추가하여 main에 탑재

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle()
+    .setDescription()
+    .setVersion('1.0')
+    .addTags('Toy-Squads')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
