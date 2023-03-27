5177443deac1d92231e5acf994922715e218c1e7
eunkangchoi <ekk12mv2@gmail.com>
2023-03-27 19:12:20 +0900

    :sparkles: feat: mysql 모듈 추가
    mysql 관련 모듈과 서비스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MysqlModule } from './mysql/mysql.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -13,6 +14,7 @@ import * as Joi from 'joi';
         SERVER_PORT: Joi.number().default(3001).required(),
       }),
     }),
+    MysqlModule,
   ],
   controllers: [AppController],
   providers: [AppService],
