7e73b93d45288c2b1418aa803515675276b8a4dd
loveAlakazam <dmsrkd1216@gmail.com>
2023-03-29 23:42:48 +0900

    :wrench: chore: db관련 설정값 추가
    데이터베이스 관련 설정값을 app.module에 추가하였음.

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,12 @@ import { LoggersMiddleware } from './loggers/loggers.middleware';
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('development', 'production').required(),
         SERVER_PORT: Joi.number().default(3000).required(),
+        /* DATABASE (RDBMS) */
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
       }),
     }),
     MysqlModule,
