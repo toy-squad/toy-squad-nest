3c80548b292a5c936db3c28140b493e811816c50
eunkangchoi <ekk12mv2@gmail.com>
2023-03-29 18:42:30 +0900

    :sparkles: init: logger 미들웨어 추가
    로거 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MysqlModule } from './mysql/mysql.module';
+import { LoggersModule } from './loggers/loggers.module';
 import * as Joi from 'joi';
+import { LoggersMiddleware } from './loggers/loggers.middleware';
 
 @Module({
   imports: [
@@ -11,12 +13,20 @@ import * as Joi from 'joi';
       envFilePath: '.env',
       isGlobal: true,
       validationSchema: Joi.object({
-        SERVER_PORT: Joi.number().default(3001).required(),
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+        SERVER_PORT: Joi.number().default(3000).required(),
       }),
     }),
     MysqlModule,
+    LoggersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    if (process.env.NODE_ENV !== 'production') {
+      consumer.apply(LoggersMiddleware).forRoutes('*');
+    }
+  }
+}
