e49bf37be5d126f6f875fdd84ba34f49e9fec31e
loveAlakazam <dmsrkd1216@gmail.com>
2023-04-17 21:03:19 +0900

    :truck: rename: local-auth.guard 로 이름변경
    local.guard -> local-auth 로 변경

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalGuard implements CanActivate {
+export class LocalAuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
