8731a0eda54f50ea5a38b28eab7115f2a65dbe3f
loveAlakazam <dmsrkd1216@gmail.com>
2023-06-21 21:53:11 +0900

    :sparkles: feat: 회원조회 repository단 로직 작성

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserRequestDto } from './dtos/create-user-request.dto';
+import { FindUserRequestDto } from './dtos/find-user-request.dto';
 
 @Injectable()
 export class UsersRepository {
@@ -12,6 +17,28 @@ export class UsersRepository {
     try {
       const { positionCategory, ...newUser } = dto;
       return await this.repo.save(newUser);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  async findUser(dto: FindUserRequestDto) {
+    try {
+      const { email, userId } = dto;
+      if (!email || !userId) {
+        throw new BadRequestException(
+          '회원 조회를 위한 조건이 존재하지 않습니다.',
+        );
+      }
+      const user = await this.repo.find({
+        where: {
+          id: userId,
+          email: email,
+        },
+      });
+
+      return user;
     } catch (err) {
       console.error(err);
       throw err;
