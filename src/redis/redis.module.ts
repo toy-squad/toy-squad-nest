d0dd7081611f6510faec4a5a5038f364e8baf376
loveAlakazam <dmsrkd1216@gmail.com>
2023-06-26 20:17:04 +0900

    :pencil2: chore: Redis모듈정의 (작업중...)

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
 
 @Module({
+  imports: [],
   providers: [RedisService],
+  exports: [],
 })
 export class RedisModule {}
