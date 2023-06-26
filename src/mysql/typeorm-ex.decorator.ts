c7b3867a396444a769c13e543683ed7c37b326e9
loveAlakazam <dmsrkd1216@gmail.com>
2023-06-26 11:08:19 +0900

    :art: chore: tsconfig.json 기본경로 수정
    src를 루트디렉토리로 설정

--- src/mysql/typeorm-ex.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const TYPEORM_EX_REPO = 'TYPEORM_EX_REPO';
-
-export function CustomRepository(entity: Function): ClassDecorator {
-  return SetMetadata(TYPEORM_EX_REPO, entity);
-}
