384cd220c0bbe853d9a0407a560524ee1556a79b
loveAlakazam <dmsrkd1216@gmail.com>
2023-07-04 03:23:38 +0900

    :sparkles: fix: product 엔티티컬럼 수정

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { CoreEntity } from '../../commons/entities/core.entity';
 import { SKILL_TYPE } from '../../users/types/skill.type';
 import { Users } from 'users/entities/user.entity';
+import { SKILL_TYPE_SET } from 'commons/types/skill.type';
 
 @Entity({ schema: process.env.DB_NAME })
 export class Project extends CoreEntity {
@@ -19,12 +20,15 @@ export class Project extends CoreEntity {
   })
   intro: string;
 
-  @Column({
+  /**
+   * 프로젝트 주요기술
+   */
+  @Column('simple-array', {
     name: 'skills',
-    nullable: false,
+    nullable: true,
     comment: '프로젝트 주요 기술',
   })
-  skills: string;
+  skills: string[];
 
   @Column({
     name: 'img_url',
@@ -65,28 +69,69 @@ export class Project extends CoreEntity {
   })
   period: string;
 
-  @Column({
+  /**
+   * 프로젝트 분야
+   * - 유저가 생성하는 컨텐츠
+   * - 동적데이터
+   */
+  @Column('simple-array', {
     name: 'field',
-    nullable: false,
+    nullable: true,
+    default: null,
     comment: '프로젝트 분야',
   })
-  field: string;
+  field: string[];
 
+  /**
+   * 프로젝트 타입
+   * - 프로젝트 플랫폼
+   * - 모바일, 앱, 데스크탑 앱, 프로그램, 웹, 게임
+   */
   @Column({
     name: 'type',
     nullable: false,
-    comment: '프로젝트 형태',
+    comment: '프로젝트 타입',
   })
   type: string;
 
+  /**
+   * 프로젝트 대면/비대면 여부
+   * - 대면: true
+   * - 비대면: false
+   */
   @Column({
-    name: 'post',
-    nullable: false,
-    comment: '게시글',
+    name: 'contact',
+    default: true,
+    comment: '프로젝트 대면/비대면 여부',
   })
-  post: string;
+  contact: boolean;
 
-  @ManyToOne(() => Users, (user) => user.project, { onDelete: 'CASCADE' })
+  /**
+   * 프로젝트 총 기간
+   */
+  @Column({
+    name: 'duration',
+    nullable: true,
+    default: 0,
+    comment: '프로젝트 기간',
+  })
+  duration: number;
+
+  /**
+   * 모집마감일
+   */
+  @Column({
+    name: 'recruit_end_date',
+    nullable: true,
+    default: null,
+    comment: '모집마감일',
+  })
+  recruitEndDate: string;
+
+  /**
+   * 프로젝트 팀장(유저) : 프로젝트 = 1:N
+   */
+  @ManyToOne(() => Users, (user) => user.project)
   @JoinColumn({ name: 'leader_id', referencedColumnName: 'id' })
   user: Users;
 
