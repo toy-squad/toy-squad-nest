7e8d45081fc35213d1409e6c8fa5bfcefee9010f
loveAlakazam <dmsrkd1216@gmail.com>
2023-07-19 01:58:49 +0900

    :wrench: Feat: 레디스모듈 환경 정의
    레디스 패키지 및 RedisInsight에서 연결확인하여 레디스환경셋팅관련 정보 기입

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import * as ioRedis from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    ioRedis.RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        config: {
+          host: configService.get('REDIS_URL'),
+          port: +configService.get('REDIS_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+  ],
   providers: [RedisService],
-  exports: [],
+  exports: [RedisService],
 })
 export class RedisModule {}
