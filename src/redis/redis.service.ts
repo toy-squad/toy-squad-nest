de5573babf803eb0d6f84c3d0cb1b88d00ed23b1
eunkang-dyphi <eunkang@dyphi.com>
2023-07-19 16:30:37 +0900

    :sparkles: feat: Redis를 사용하는 서비스 로직 추가

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async set() {
+    return await this.redis.set('key', 'value', 'EX', 10);
+  }
+}
