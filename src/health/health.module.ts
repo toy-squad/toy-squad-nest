4e0b7f738a376e2df6cb5ed7de20101f65e8380e
eunkang-dyphi <eunkang@dyphi.com>
2023-07-19 16:30:02 +0900

    :heavy_plus_sign: chore: HealthModule 의존 모듈 추가
    Axios를 통한 HTTP 통신을 위한 하위모듈 HttpModule 추가

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TerminusModule, ConfigModule],
+  imports: [TerminusModule, ConfigModule, HttpModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
