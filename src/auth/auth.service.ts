6c070e3147021677a0b0c2f81545f3307367de8b
loveAlakazam <dmsrkd1216@gmail.com>
2023-08-20 00:20:44 +0900

    :sparkles: Feat: 로그인/로그아웃/토큰요청시 레디스에서 토큰값 읽기/쓰기 요청 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,31 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  UnauthorizedException,
+  UseInterceptors,
+} from '@nestjs/common';
 import { EmailService } from 'email/email.service';
 import { UsersService } from 'users/users.service';
 import { ValidateUserRequestDto } from './dtos/requests/validate-user-request.dto';
 import { ConfirmPasswordRequestDto } from 'users/dtos/requests/confirm-password-request.dto';
 import { PublicUserInfo } from 'users/types/public-user-info.type';
 import { JwtService } from '@nestjs/jwt';
+import { Cache } from 'cache-manager';
 import { ConfigService } from '@nestjs/config';
+import { CACHE_MANAGER, CacheInterceptor } from '@nestjs/cache-manager';
 
 @Injectable()
 export class AuthService {
+  private TOKEN_EXPIRATION: number;
   constructor(
     private readonly userService: UsersService,
     private readonly emailService: EmailService,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
-  ) {}
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {
+    this.TOKEN_EXPIRATION = +this.configService.get('ACCESS_TOKEN_EXPIRATION');
+  }
 
   // 유저 검증
   // Todo: 이메일 2차인증 추가예정
@@ -42,6 +53,7 @@ export class AuthService {
    * JWT 토큰 생성
    * @param payload : payload는 토큰에 담을 정보. 유저귀중정보를 제외한 나머지
    */
+  @UseInterceptors(CacheInterceptor)
   async signIn(userId: string, email: string) {
     try {
       // Payload:  JWT Payload 에는 토큰에 담을 정보가 들어있다.
@@ -50,15 +62,33 @@ export class AuthService {
       // 액세스토큰을 생성한다
       const accessToken = this.jwtService.sign(payload);
 
-      // 생성된 액세스토큰을 레디스에 셋한다.
+      // 생성된 액세스토큰을 레디스에 저장한다
       // 키: 유저아이디
       // 값: 액세스토큰
       // 유효기간: 토큰 유효기간(.env에 정의된 값)
+      await this.cacheManager.set(userId, accessToken, this.TOKEN_EXPIRATION);
 
       //JWT토큰을 리턴
       return {
         access_token: accessToken,
       };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logOut(userId: string) {
+    try {
+      // userId가 undefined가 아닌 유효한값인지 확인한다.
+      // 캐시에 userId인 키가 있는지 확인한다.
+      const accessToken = await this.cacheManager.get(userId);
+      const isUnvalidUser = !userId || !accessToken;
+      if (isUnvalidUser) {
+        throw new UnauthorizedException('유효하지 않은 회원입니다.');
+      }
+
+      // 캐시히트가 된다면, 레디스에 있는 값을 지운다.
+      await this.cacheManager.del(userId);
     } catch (error) {
       throw error;
     }
@@ -71,8 +101,6 @@ export class AuthService {
   public getCookieWithJwtToken(userId: string, email: string) {
     const payload: TokenPayload = { userId: userId, email: email };
     const token = this.jwtService.sign(payload);
-    return `access_token=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get(
-      'JWT_EXPIRATION',
-    )}`;
+    return `access_token=${token}; HttpOnly; Path=/; Max-Age=${this.TOKEN_EXPIRATION}`;
   }
 }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { ContextIdFactory, ModuleRef } from '@nestjs/core';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { Strategy, ExtractJwt } from 'passport-jwt';
@@ -7,7 +8,10 @@ import { UsersService } from 'users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly usersService: UsersService) {
+  constructor(
+    private readonly usersService: UsersService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -15,16 +19,25 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(request: Request, payload: TokenPayload) {
+  async validate(payload: TokenPayload) {
     // payload가 유효한지 확인한다.
-    console.log(request.user);
 
     const { userId, email } = payload;
-    const signInUser = await this.usersService.findOneUser({
+
+    // [todo] 레디스에 key(userId) 가 캐시히트되는지 확인 -> 캐시 히트안되면 로그인 에러처리한다.
+    const access_token = await this.cacheManager.get(userId);
+    if (!access_token) {
+      throw new UnauthorizedException(
+        '유저토큰의 유효기간이 지났습니다. 다시 로그인해주세요.',
+      );
+    }
+    // 로그인하려는 유저가 올바른 유저인지 확인한다.
+    const user = await this.usersService.findOneUser({
       userId: userId,
       email: email,
     });
-    return signInUser;
+
+    return user;
   }
 
   private extractTokenFromHeader(request: Request) {
