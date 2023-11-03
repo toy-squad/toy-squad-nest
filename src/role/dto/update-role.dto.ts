ba0f9fabbbd61e07f127484efde2560cbb860daa
johny <jskjw157@gmail.com>
2023-11-03 17:18:02 +0900

    🎨 Role Update DTO
    - 불필요한 내용 삭제

--- src/role/dto/update-role.dto.ts
+++ src/role/dto/update-role.dto.ts
@@ -1,14 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {
-
-    // @IsNotEmpty()
-    // @IsString()
-    // projectId: string;
-
-    // @IsNotEmpty()
-    // @IsString()
-    // userId: string;
-}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
