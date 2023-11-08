07d37552d322d6c3c5553221250fd893f895d4b6
johny <jskjw157@gmail.com>
2023-11-08 19:32:11 +0900

    📝 Add Swagger API documentation to project controller

--- src/projects/dtos/requests/create-new-project.dto.ts
+++ src/projects/dtos/requests/create-new-project.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import {
   IsArray,
@@ -31,10 +32,12 @@ export interface ICreateProjectDto {
  */
 export class CreateNewProjectRequestDto implements ICreateProjectDto {
   // 유저 //
+  @ApiProperty({ description: '유저 아이디(로그인)', required: true })
   @IsNotEmpty()
   userId: string; // required
 
   // 권한
+  @ApiProperty({ description: '권한', default: 'G' })
   @IsOptional()
   @Transform(({ value }) => {
     value ?? 'G';
@@ -44,69 +47,82 @@ export class CreateNewProjectRequestDto implements ICreateProjectDto {
 
   // 프로젝트 //
   // 프로젝트이름
+  @ApiProperty({ description: '프로젝트 이름', required: true})
   @IsNotEmpty()
   @IsString()
   name: string; // required
 
   // 프로젝트소개
+  @ApiProperty({ description: '프로젝트 소개' })
   @IsOptional()
   @IsString()
   intro: string;
 
   // 프로젝트 설명
+  @ApiProperty({ description: '프로젝트 설명' })
   @IsOptional()
   @IsString()
   description: string;
 
   // 프로젝트 주요기술
+  @ApiProperty({ description: '프로젝트 주요 기술' })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   skills: string[];
 
   // 프로젝트 썸네일 이미지 URL
+  @ApiProperty({ description: '프로젝트 썸네일 이미지 URL' })
   @IsOptional()
   @IsString()
   imgUrl: string;
 
   // 프로젝트 시작일
+  @ApiProperty({ description: '프로젝트 시작일' })
   @IsOptional()
   @IsString()
   startDate: string;
 
   // 프로젝트 종료일
+  @ApiProperty({ description: '프로젝트 종료일' })
   @IsOptional()
   @IsString()
   endDate: string;
 
   // 프로젝트 완성품 타입
+  @ApiProperty({ description: '프로젝트 완성품 타입' })
   @IsOptional()
   @IsString()
   productType: string;
 
   // 프로젝트 분야
+  @ApiProperty({ description: '프로젝트 분야' })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   fields: string[];
 
   // 프로젝트 대면/비대면 여부
+  @ApiProperty({ description: '프로젝트 대면/비대면 여부' })
   @IsOptional()
   @IsString()
   @IsEnum(contactType)
   contactType: contactType;
 
   //  프로젝트 활동 장소
+  @ApiProperty({ description: '프로젝트 활동 장소' })
   @IsOptional()
   @IsString()
   place: string;
 
   //  프로젝트 모집 시작일
+  @ApiProperty({ description: '프로젝트 모집 시작일' })
   @IsOptional()
   @IsString()
   recruitStartDate: string;
 
   //  프로젝트 모집 마감일
+  @ApiProperty({ description: '프로젝트 모집 마감일' })
   @IsOptional()
   @IsString()
   recruitEndDate: string;
--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -17,21 +17,39 @@ import RequestWithUser from 'auth/interfaces/request-with-user.interface';
 import { Response } from 'express';
 import { CreateNewProjectRequestDto } from './dtos/requests/create-new-project.dto';
 import { GetProjectsRequestDto } from './dtos/requests/get-projects-request.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiProperty, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Project } from './entities/project.entity';
 
+@ApiTags('project')
 @Controller('project')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
+  @ApiOperation({ summary: '프로젝트 단건 조회' })
+  @ApiResponse({  status: 200, description: '프로젝트 단건 조회 성공.', type: Project})
+  @ApiParam({ name: 'id', description: '프로젝트 아이디', required: true })
   @Get(':id')
   async findOneProject(
     @Param('id') id: string,
     @Req() request: RequestWithUser,
     @Res() response: Response,
   ) {
-    const project = await this.projectsService.findOneProject(id);
-    return response.json(project);
+    try {
+      const project = await this.projectsService.findOneProject(id);
+      return response.status(HttpStatus.OK).json(project);
+
+    } catch (error) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: '프로젝트 조회에 실패했습니다.',
+        error: error.message,
+      });
+    }
   }
 
+  @ApiOperation({ summary: '프로젝트 목록 조회' })
+  @ApiResponse({status: 200, description: '프로젝트 목록 조회 성공.', type: [Project]})
+  @ApiQuery({ name: 'page', description: '페이지 번호',type: Number, required: false })
+  @ApiQuery({ name: 'limit', description: '페이지당 데이터 갯수', type: Number, required: false })
   @Get('list')
   async getProjects(
     @Query() reqDto: GetProjectsRequestDto,
@@ -49,54 +67,80 @@ export class ProjectsController {
     }
   }
 
-
+  @ApiProperty({ description: '프로젝트 생성' })
+  @ApiResponse({ status: 201, description: '프로젝트 생성 성공.', type: Project })
+  @ApiBody({ type: CreateNewProjectRequestDto })
   @Post()
   async createNewProject(
     @Req() request: RequestWithUser,
     @Res() response: Response,
   ) {
-    const bodyInfo = request.body;
-    const userId = request.user.userId;
+    try {
+      const reqDto: CreateNewProjectRequestDto = {
+        userId: request.user.userId,
+        ...request.body
+      };
+  
+      const newProject = await this.projectsService.createNewProject(reqDto);
+      
+      return response.status(HttpStatus.OK).json(newProject);
+    } catch (error) {
 
-    const newProject = await this.projectsService.createNewProject({
-      userId: userId,
-      ...bodyInfo,
-    });
-    return response.json(newProject);
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: '프로젝트 생성에 실패했습니다.',
+        error: error.message,
+      });
+    }
   }
 
-  @Patch(':id')
+  @ApiOperation({ summary: '프로젝트 수정' })
+  @ApiResponse({ status: 200, description: '프로젝트 수정 성공.', type: Project })
+  @ApiBody({ type: UpdateProjectRequestDto })
+  @Patch()
   async updateProject(
-    @Param('id') projectId: string,
     @Req() request: RequestWithUser,
     @Res() response: Response,
   ) {
-    const userId = request.user.userId;
-    const bodyInfo = request.body;
+    try {
 
-    const updatedProject = await this.projectsService.updateProject({
-      projectId,
-      userId,
-      ...bodyInfo,
-    });
+      const reqDto: UpdateProjectRequestDto = {
+        userId: request.user.userId,
+        ...request.body
+      };
+      
+      const updatedProject = await this.projectsService.updateProject(reqDto);
+      return response.status(HttpStatus.OK).json(updatedProject);
+    
+    } catch (error) {
 
-    return response.json(updatedProject);
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: '프로젝트 수정에 실패했습니다.',
+        error: error.message,
+      });
+    }
   }
 
-  @Delete(':id')
+  @ApiOperation({ summary: '프로젝트 삭제' })
+  @ApiResponse({ status: 200, description: '프로젝트 삭제 성공.', type: Project })
+  @ApiBody({ type: UpdateProjectRequestDto })
+  @Delete()
   async deleteProject(
-    @Param('id') projectId: string,
     @Req() request: RequestWithUser,
     @Res() response: Response,
   ) {
-    const userId = request.user.userId;
-    const bodyInfo = request.body;
-
-    const deleteProject = await this.projectsService.softDeleteProject({
-      projectId,
-      userId,
-      ...bodyInfo,
-    });
-    return response.json(deleteProject);
+    try {
+      const reqDto: UpdateProjectRequestDto = {
+        userId: request.user.userId,
+        ...request.body
+      };
+  
+      const deleteProject = await this.projectsService.softDeleteProject(reqDto);
+      return response.status(HttpStatus.OK).json(deleteProject);
+    } catch (error) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: '프로젝트 삭제에 실패했습니다.',
+        error: error.message,
+      });
+    }
   }
 }
