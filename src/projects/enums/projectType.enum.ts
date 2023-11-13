24118a57c8da39550ca8203d5eeaf6a1abb5a9fb
johny <jskjw157@gmail.com>
2023-11-13 14:13:22 +0900

    ✨ Refactor projectType.enum.ts to include firstPosition enum

--- src/projects/enums/projectType.enum.ts
+++ src/projects/enums/projectType.enum.ts
@@ -1,5 +1,5 @@
 /**
- * 프로젝트 대면/비대면/혼합
+ * 프로젝트 진행 방식 종류
  * - C  : 대면(Contact)
  * - U  : 비대면(Untact)
  * - M  : 대면/비대면 혼합방식(Mix)
@@ -8,4 +8,16 @@ export enum contactType {
   CONTACT = 'C',
   UNTACT = 'U',
   MIX = 'M',
+}
+
+/**
+ * 포지션 종류
+ * - MANAGER  : 매니저
+ * - DEVELOPER  : 개발자
+ * - DESIGNER  : 디자이너
+ */
+export enum firstPosition {
+  MANAGER = 'MANAGER',
+  DEVELOPER = 'DEVELOPER',
+  DESIGNER = 'DESIGNER',
 }
