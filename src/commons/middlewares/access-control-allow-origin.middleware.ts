ff6ea9c30ccfefb7c4808628549b0ab63631e1ec
loveAlakazam <dmsrkd1216@gmail.com>
2023-11-23 03:18:51 +0900

    :bug: Fix: cors에러 수정 (#46)

--- src/commons/middlewares/access-control-allow-origin.middleware.ts
+++ src/commons/middlewares/access-control-allow-origin.middleware.ts
@@ -4,6 +4,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class AccessControlAllowOriginMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Origin', 'https://accounts.google.com');
     res.setHeader('Access-Control-Allow-Origin', 'https://accounts.kakao.com');
     next();
