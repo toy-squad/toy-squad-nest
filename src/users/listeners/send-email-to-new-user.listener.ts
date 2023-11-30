58da543221f84876537b7fe3da782cd7adbaf501
loveAlakazam <dmsrkd1216@gmail.com>
2023-12-01 02:16:18 +0900

    :arrow_up: Update: 의존성 수정 EventEmitter을 AppModule이 의존하도록 수정
    의존대상을 수정 usersModule -> appModule

--- src/users/listeners/send-email-to-new-user.listener.ts
+++ src/users/listeners/send-email-to-new-user.listener.ts
@@ -1,10 +1,11 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { SendEmailToNewUserEvent } from 'users/events/send-email-to-new-user.event';
 
 @Injectable()
 export class SendEmailToNewUserListener {
-  constructor() {}
+  constructor(private readonly mailService: MailerService) {}
   @OnEvent('send.email.to.new.user')
   handleSendEmailToNewUserEvent(event: SendEmailToNewUserEvent) {}
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,14 +6,12 @@ import { User } from './entities/user.entity';
 import { UsersRepository } from './users.repository';
 import { AuthModule } from 'auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     forwardRef(() => AuthModule),
     ConfigModule,
-    EventEmitterModule.forRoot(),
   ],
   providers: [UsersService, UsersRepository],
   controllers: [UsersController],
