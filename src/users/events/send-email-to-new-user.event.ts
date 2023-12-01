02e416724f26bd9bb2aadad95d86c97044c72c18
loveAlakazam <dmsrkd1216@gmail.com>
2023-12-02 02:04:55 +0900

    :necktie: Add: 회원가입완료후 가입환영이메일 이벤트 작성
    회원가입완료후 가입환영이벤트와 이벤트리스너 작성

--- src/users/events/send-email-to-new-user.event.ts
+++ src/users/events/send-email-to-new-user.event.ts
@@ -1,4 +1,7 @@
 export class SendEmailToNewUserEvent {
   // 신규회원 이메일
   email: string;
+
+  // 신규회원 이름
+  name: string;
 }
--- src/users/listeners/send-email-to-new-user.listener.ts
+++ src/users/listeners/send-email-to-new-user.listener.ts
@@ -1,11 +1,30 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { OnEvent } from '@nestjs/event-emitter';
 import { SendEmailToNewUserEvent } from 'users/events/send-email-to-new-user.event';
 
 @Injectable()
 export class SendEmailToNewUserListener {
-  constructor(private readonly mailService: MailerService) {}
+  constructor(
+    private readonly mailService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
   @OnEvent('send.email.to.new.user')
-  handleSendEmailToNewUserEvent(event: SendEmailToNewUserEvent) {}
+  async handleSendEmailToNewUserEvent(event: SendEmailToNewUserEvent) {
+    try {
+      // 회원가입 회원 이메일을 전송한다.
+      await this.mailService.sendMail({
+        to: event.email,
+        from: 'admin@toysquad.com',
+        subject: '토이스쿼드에 가입해주셔서 감사합니다',
+        template: '../../email/templates/greet_new_user.hbs',
+        context: {
+          userName: event.name,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
 }
