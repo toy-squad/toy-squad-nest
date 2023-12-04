ffa34eb0fac953c915ab6823388884bfa6190b70
johny <jskjw157@gmail.com>
2023-12-04 16:05:00 +0900

    ✅ 댓글 모듈 repository 설정

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,11 +3,12 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
+import { CommentRepository } from './comment.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment])],
   exports: [TypeOrmModule],
   controllers: [CommentController],
-  providers: [CommentService]
+  providers: [CommentService, CommentRepository]
 })
 export class CommentModule {}
