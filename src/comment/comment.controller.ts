9dc220175d5663f216bfc4de8b424f9c0a5a1bcc
eunkang-dyphi <eunkang@dyphi.com>
2023-12-13 20:26:04 +0900

    :card_file_box: Update: Comment 엔티티 수정

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -7,28 +15,47 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  // 댓글작성
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentService.create(createCommentDto);
+  async createComment(@Body() createCommentDto: CreateCommentDto) {
+    return await this.commentService.create(createCommentDto);
   }
 
+  // 대댓글 작성
+  @Post(':id/reply')
+  async generateReplyComment() {}
+
+  // 대댓글에 해시태그 답글작성
+  @Post(':id/reply/hashtag')
+  async generateHashtagReplyComment() {}
+
+  // 프로젝트 댓글 전체 조회
   @Get()
-  findAll() {
-    return this.commentService.findAll();
+  async findAll() {
+    return await this.commentService.findAll();
   }
 
+  // 댓글(대댓글) id로 조회
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commentService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.commentService.findOne(+id);
   }
+  // 프로젝트 id로 대댓글 조회
+  @Get(':id/children')
+  async getReplyComments() {}
 
+  // 댓글 수정
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ) {
+    return await this.commentService.update(+id, updateCommentDto);
   }
 
+  // 댓글 삭제 (softDelete)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commentService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.commentService.remove(+id);
   }
 }
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,65 +1,88 @@
-import { CoreEntity } from "commons/entities/core.entity";
-import { Project } from "projects/entities/project.entity";
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
-import { User } from "users/entities/user.entity";
+import { CoreEntity } from 'commons/entities/core.entity';
+import { Project } from 'projects/entities/project.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { User } from 'users/entities/user.entity';
 
-@Entity({schema: process.env.DB_NAME})
-export class Comment extends CoreEntity{
+@Entity({ schema: process.env.DB_NAME })
+export class Comment extends CoreEntity {
+  /** 댓글, 대댓글 모두 공통으로 갖는 필드 */
+  // 댓글 내용
+  @Column('text')
+  content: string;
 
-    // 댓글 내용
-    @Column('text')
-    content: string;
+  // 좋아요 수
+  @Column({ default: 0 })
+  likes: number;
 
-    /**
-     * 대댓글 기능
-     * 대댓글 기능을 위해 부모 댓글을 지정한다.
-     * 부모 댓글이 없으면 null
-     * 부모 댓글이 있으면 부모 댓글의 id
-     */
-    @ManyToOne(() => Comment, (comment) => comment.children, { 
-        nullable: true, 
-        onDelete: 'CASCADE' 
-    })
-    @JoinColumn({ name: 'parentId' })
-    parent: Comment;
+  // 싫어요 수
+  @Column({ default: 0 })
+  dislikes: number;
 
-    /**
-     * 대댓글 기능
-     * 대댓글 기능을 위해 자식 댓글을 지정한다.
-     * 자식 댓글이 없으면 null
-     * 자식 댓글이 있으면 자식 댓글의 id
-     */
-    @OneToMany(() => Comment,(comment) => comment.parent, {
-        nullable: true, 
-        eager: true
-    })
-    children: Comment[];
+  /**
+   * 댓글:프로젝트 = N:1
+   *
+   */
+  @ManyToOne(() => Project, (project) => project.comments, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'projectId' })
+  project: Project;
 
-    // 좋아요 수
-    @Column({ default: 0 })
-    likes: number;
+  /**
+   * 유저: 댓글작성자
+   * 댓글:유저 = N:1
+   */
+  @ManyToOne(() => User, (user) => user.comments, {
+    onDelete: 'CASCADE',
+    eager: true,
+  })
+  user: User;
 
-    // 싫어요 수
-    @Column({ default: 0 })
-    dislikes: number;
+  // --------------------------------------------------------
+  /**
+   * 대댓글 기능
+   * 대댓글 기능을 위해 부모 댓글을 지정한다.
+   * 부모 댓글이 없으면 null
+   * 부모 댓글이 있으면 부모 댓글의 id
+   */
+  @ManyToOne(() => Comment, (comment) => comment.children, {
+    nullable: true,
+    // onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'parentId' })
+  parent: Comment;
 
-    /**
-     * 댓글:프로젝트 = N:1
-     * 
-     */
-    @ManyToOne(() => Project, (project) => project.comments, { 
-        onDelete: 'CASCADE' 
-    })
-    @JoinColumn({ name: 'projectId' })
-    project: Project;
+  /**
+   * 대댓글 기능
+   * 대댓글 기능을 위해 자식 댓글을 지정한다.
+   * 자식 댓글이 없으면 null
+   * 자식 댓글이 있으면 자식 댓글의 id
+   */
+  @OneToMany(() => Comment, (comment) => comment.parent, {
+    nullable: true,
+    eager: true,
+  })
+  children: Comment[];
 
-    /**
-     * 댓글:유저 = N:1
-     */
-    @ManyToOne(() => User, (user) => user.comments, {
-        onDelete: 'CASCADE',
-        eager: true
-    })
-    user: User;
+  /**
+   * 대댓글 기능
+   * - 대댓글에 댓글을 작성한경우
+   * - 해시태그는 대댓글의 아이디를 참조한다
+   */
+  @OneToMany(() => Comment, (comment) => comment.hashtagTarget, {
+    nullable: true,
+  })
+  hashtagComments: Comment[];
 
+  @ManyToOne(() => Comment, (comment) => comment.hashtagComments)
+  @JoinColumn()
+  hashtagTarget: Comment;
 }
