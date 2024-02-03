b3f854e67bddf2e620d6bee2e364f0dcde7d4a61
loveAlakazam <eunkang@dyphi.com>
2024-02-04 03:20:16 +0900

    👔 Update: 이미지URL 파싱하여 Key값 추출하는 함수생성

--- src/commons/constants/FILE_CONSTANT.ts
+++ src/commons/constants/FILE_CONSTANT.ts
@@ -1,2 +1,16 @@
 export const UPLOAD_IMAGE_MAX_SIZE = 1024 * 1024 * 10; // 10MB
 export const VALID_IMAGE_FILE_TYPES_REGEX = /^image\/(jpg|jpeg|png|gif)$/;
+
+// mimeType 초기값 image/jpg|jpeg|png|gif  => jpg|jpeg|png|gif 로 변환한다.
+export const getImageFileTypeFromMimeType = (inputImageMimeType: string) => {
+  return inputImageMimeType.replace('image/', '');
+};
+
+// s3 imgUrl = https://s3.${this.AWS_REGION}.amazonaws.com/${this.AWS_BUCKET_NAME}/${dirName}/${fileName}
+// key (${dirName}/${fileName}) 를 파싱한다.
+export const getKeyFromS3Url = (inputS3ImageUrl: string) => {
+  return inputS3ImageUrl.replace(
+    `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_BUCKET_NAME}/`,
+    '',
+  );
+};
