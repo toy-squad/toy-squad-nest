6080f701803bb5d754f6fcf7f296fc3c5919118b
loveAlakazam <eunkang@dyphi.com>
2024-02-16 02:19:22 +0900

    :card_file_box: Update: likes 모듈에 likes엔티티 연결 및 의존성 추가

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Likes } from './entities/likes.entity';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Likes]), ConfigModule],
+  providers: [],
+  exports: [],
+})
 export class LikesModule {}
