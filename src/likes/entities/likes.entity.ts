4a8923ba6784a8e050c9401ef8793a33a63fcbd1
loveAlakazam <eunkang@dyphi.com>
2024-02-22 01:07:24 +0900

    :card_file_box: Fix: likes.entity 수정 - 다중복합키 를 나타내기위한 데코레이터 수정

--- src/likes/entities/likes.entity.ts
+++ src/likes/entities/likes.entity.ts
@@ -1,15 +1,15 @@
 import { CoreEntity } from 'commons/entities/core.entity';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryColumn } from 'typeorm';
 
 @Entity({ schema: process.env.DB_NAME })
 export class Likes extends CoreEntity {
-  @PrimaryGeneratedColumn({
+  @PrimaryColumn({
     name: 'from',
     comment: '좋아요를 준 유저의 PK',
   })
   from: string;
 
-  @PrimaryGeneratedColumn({
+  @PrimaryColumn({
     name: 'to',
     comment: '좋아요를 받은 유저의 PK',
   })
