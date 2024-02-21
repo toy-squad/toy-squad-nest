138aa7b8156340de1928ab534ecbf89d0ea5655b
loveAlakazam <eunkang@dyphi.com>
2024-02-22 01:08:12 +0900

    :heavy_plus_sign: Add: 모듈에 LikesRepository 의존성 추가

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Likes } from './entities/likes.entity';
 import { ConfigModule } from '@nestjs/config';
+import { LikesRepository } from './likes.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Likes]), ConfigModule],
-  providers: [],
-  exports: [],
+  providers: [LikesRepository],
+  exports: [LikesRepository],
 })
 export class LikesModule {}
