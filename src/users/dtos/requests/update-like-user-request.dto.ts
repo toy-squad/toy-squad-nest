f2afa690d819fdb034d9554cdc078d047dca6a65
loveAlakazam <eunkang@dyphi.com>
2024-02-22 01:10:21 +0900

    :label: Update: UpdateLikeUser Dto 분리 - 컨트롤러Dto(UpdateLikeUserRequestDto)와 ServiceDto(UpdateLikeUserServiceRequestDto)로 분리

--- src/users/dtos/requests/update-like-user-request.dto.ts
+++ src/users/dtos/requests/update-like-user-request.dto.ts
@@ -6,7 +6,23 @@ import { ApiProperty } from '@nestjs/swagger';
  * - CANCEL : 좋아요 취소
  */
 type likeType = 'LIKE' | 'CANCEL';
+
 export class UpdateLikeUserRequestDto {
+  @ApiProperty({
+    required: true,
+    description: '좋아요(좋아요 취소) 를 받은 유저 아이디',
+    example: 'c2e3a923-7cdb-4716-9caa-839f22639e68',
+  })
+  to: string;
+
+  @ApiProperty({
+    required: true,
+    description: '좋아요 유형 (LIKE: 좋아요, CANCEL: 좋아요 취소)',
+    example: 'LIKE',
+  })
+  likeType: likeType;
+}
+export class UpdateLikeUserServiceRequestDto {
   @ApiProperty({
     required: true,
     description: '좋아요(좋아요 취소)를 준 유저아이디 : 로그인 유저',
@@ -25,5 +41,5 @@ export class UpdateLikeUserRequestDto {
     description: '좋아요 유형 (LIKE: 좋아요, CANCEL: 좋아요 취소)',
     example: 'LIKE',
   })
-  likeType: likeType = 'LIKE';
+  likeType: likeType;
 }
